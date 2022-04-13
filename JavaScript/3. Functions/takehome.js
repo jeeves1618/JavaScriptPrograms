c36c321f39d2c266270d426d1bc7c692a9217fc1
Vijay Ponnusamy <jeeves1618@gmail.com>
2022-04-13 07:18:19 +0530

    Learning JavaScript

--- JavaScript/3. Functions/takehome.js
+++ JavaScript/3. Functions/takehome.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 let salarySlab = [250000, 250000, 500000, 1000000];
 let taxRate = [0.0, 5.2, 20.8, 31.2];
@@ -9,39 +9,95 @@ let employmentTax = 2500.0;
 let taxSurchargeFactor = 1;
 let totalTax = 0;
 
-let annualGrossSalary = prompt("What is your Annual Gross Salary?");
+//let annualGrossSalary = prompt("What is your Annual Gross Salary?");
+let annualGrossSalary = 1300000;
+console.log(document.getElementById("grossSalaryEntered"));
 
-let taxableAmount = annualGrossSalary - houseLoanInt - section80c -standardDeduction -employmentTax;
+let taxableAmount =
+  annualGrossSalary -
+  houseLoanInt -
+  section80c -
+  standardDeduction -
+  employmentTax;
 
 if (taxableAmount >= 5000000) taxSurchargeFactor = 1.1;
 
-let grossSalary = annualGrossSalary/12;
+let grossSalary = annualGrossSalary / 12;
 
 let basicSalary = grossSalary * 0.4;
 let pf = basicSalary * 0.12;
 
-let grossSalaryFmtd = "Rs. " + grossSalary; 
-let basicSalaryFmtd = "Rs. " + basicSalary;
-let pfFmtd = "Rs. " + pf;
+let formatAmount = function (amount) {
+  let amountString = amount.toFixed(2).toString();
+  let lengthOfString = amountString.length;
+  let amountFormatter = [
+    "n",
+    "n",
+    ".",
+    "n",
+    "n",
+    "n",
+    ",",
+    "n",
+    "n",
+    ",",
+    "n",
+    "n",
+    ",",
+    "n",
+    "n",
+    ",",
+    "n",
+    "n",
+    ",",
+    "n",
+    "n",
+  ];
+  let amountFormatterItr = 0;
+  let cAmountString = " ";
+  let charAtString;
 
-for(let i = 0; i < 5 && taxableAmount > 0; i++){
-    totalTax = totalTax + (salarySlab[i] * taxRate[i]/100);
-    taxableAmount = taxableAmount - salarySlab[i];
-    if (i == 2) {
-        salarySlab[i + 1] = taxableAmount;
+  for (let i = lengthOfString - 1; i >= 0; i--) {
+    if (
+      amountFormatter[amountFormatterItr] === "n" ||
+      amountFormatter[amountFormatterItr] === "."
+    ) {
+      charAtString = amountString.charAt(i);
+    } else {
+      charAtString = amountFormatter[amountFormatterItr];
+      cAmountString = charAtString + cAmountString;
+      charAtString = amountString.charAt(i);
+      amountFormatterItr++;
     }
+    cAmountString = charAtString + cAmountString;
+    amountFormatterItr++;
+  }
+  cAmountString = "Rs. " + cAmountString;
+  return cAmountString;
+};
+
+let grossSalaryFmtd = formatAmount(grossSalary);
+let basicSalaryFmtd = formatAmount(basicSalary);
+let pfFmtd = formatAmount(pf);
+
+for (let i = 0; i < 5 && taxableAmount > 0; i++) {
+  totalTax = totalTax + (salarySlab[i] * taxRate[i]) / 100;
+  taxableAmount = taxableAmount - salarySlab[i];
+  if (i == 2) {
+    salarySlab[i + 1] = taxableAmount;
+  }
 }
 
 totalTax = totalTax * taxSurchargeFactor;
-let monthlyTax = totalTax/12;
-let monthlyTakeHome = (annualGrossSalary - totalTax - employmentTax - (pf*12))/12;
+let monthlyTax = totalTax / 12;
+let monthlyTakeHome =
+  (annualGrossSalary - totalTax - employmentTax - pf * 12) / 12;
 
-let monthlyTaxFmtd = "Rs. " + monthlyTax;
-let monthlyTakeHomeFmtd = "Rs. " + monthlyTakeHome
+let monthlyTaxFmtd = formatAmount(monthlyTax);
+let monthlyTakeHomeFmtd = formatAmount(monthlyTakeHome);
 
 document.getElementById("grossSalary").innerHTML = grossSalaryFmtd;
 document.getElementById("basicSalary").innerHTML = basicSalaryFmtd;
 document.getElementById("pf").innerHTML = pfFmtd;
 document.getElementById("monthlyTax").innerHTML = monthlyTaxFmtd;
 document.getElementById("monthlyTakeHome").innerHTML = monthlyTakeHomeFmtd;
-
